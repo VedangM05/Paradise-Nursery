79baf4e4c5c389f478adb4a3ae03097279046325
VedangM05 <mahajanvedang000@gmail.com>
2026-03-25 20:33:16 +0530

    Remove duplicate files from root directory

--- AboutUs.jsx
+++
@@ -1,8 +0,0 @@
-export default function AboutUs() {
-  return (
-    <div>
-      <h2>About Paradise Nursery</h2>
-      <p>We provide high-quality plants to beautify your home and garden.</p>
-    </div>
-  );
-}
